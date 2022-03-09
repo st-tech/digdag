ada82d4404ae040a5f2f01320aa5c10f1cc06878
Kyou13 <kyou1013@gmail.com>
2022-03-09 21:21:56 +0900

    update kubernetes-client version

--- digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClient.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/DefaultKubernetesClient.java
@@ -15,6 +15,7 @@ import io.fabric8.kubernetes.api.model.ContainerBuilder;
 import io.fabric8.kubernetes.api.model.ContainerStatus;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.EnvVarBuilder;
+import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.api.model.PodSpec;
 import io.fabric8.kubernetes.api.model.PodSpecBuilder;
 import io.fabric8.kubernetes.api.model.ResourceRequirements;
@@ -68,15 +69,16 @@ public class DefaultKubernetesClient
         final Config kubernetesPodConfig = extractTargetKindConfig(context, "Pod");
         final Container container = createContainer(context, request, kubernetesPodConfig, name, commands, arguments);
         final PodSpec podSpec = createPodSpec(context, request, kubernetesPodConfig, container);
-        io.fabric8.kubernetes.api.model.Pod pod = client.pods()
-                .createNew()
-                .withNewMetadata()
-                .withName(name)
-                .withNamespace(client.getNamespace())
-                .withLabels(getPodLabels())
-                .endMetadata()
-                .withSpec(podSpec)
-                .done();
+        final io.fabric8.kubernetes.api.model.Pod pod = client.pods().inNamespace(client.getNamespace()).create(
+               new PodBuilder()
+                   .withNewMetadata()
+                   .withName(name)
+                   .withLabels(getPodLabels())
+                   .withNamespace(client.getNamespace())
+                   .endMetadata()
+                   .withSpec(podSpec)
+                   .build()
+               );
         return Pod.of(pod);
     }
 
